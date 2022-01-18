7b60075a3c3222e5a47e15526fe9c9b910cb7747
Xiaotong Chen <xchen@habana.ai>
2022-01-18 00:35:35 -0500

    Add FusedAdamW optimizer

--- main_pretrain.py
+++ main_pretrain.py
@@ -179,7 +179,11 @@ def main(args):
     
     # following timm: set wd as 0 for bias and norm layers
     param_groups = optim_factory.add_weight_decay(model_without_ddp, args.weight_decay)
-    optimizer = torch.optim.AdamW(param_groups, lr=args.lr, betas=(0.9, 0.95))
+    if device == torch.device('hpu'):
+        from habana_frameworks.torch.hpex.optimizers import FusedAdamW
+        optimizer = FusedAdamW(param_groups, betas=(0.9, 0.95), lr=args.lr, eps=1e-8, weight_decay=1e-2)
+    else:
+        optimizer = torch.optim.AdamW(param_groups, lr=args.lr, betas=(0.9, 0.95))
     print(optimizer)
     loss_scaler = NativeScaler()
 
